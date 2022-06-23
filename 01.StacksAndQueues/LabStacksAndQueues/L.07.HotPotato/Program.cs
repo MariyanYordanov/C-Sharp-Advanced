bbb94dd1fd7de3fa652b8720ed3fc4757ca45f91
Mariyan Yordanov <marsrewq@gmail.com>
2022-06-23 05:52:45 +0300

    Add task

---
+++ 01.StacksAndQueues/LabStacksAndQueues/L.07.HotPotato/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace L._07.HotPotato
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split().ToArray();
+            Queue<string> queue = new Queue<string>(input);
+            int toss = int.Parse(Console.ReadLine());
+            int counter = 1;
+            while (queue.Count > 1)
+            {
+                string kid = queue.Dequeue();
+                if (counter % toss != 0)
+                {
+                    queue.Enqueue(kid);
+                }
+                else
+                {
+                    Console.WriteLine($"Removed {kid}");
+                }
+
+                counter++;
+            }
+
+            Console.WriteLine($"Last is {queue.Dequeue()}");
+        }
+    }
+}
