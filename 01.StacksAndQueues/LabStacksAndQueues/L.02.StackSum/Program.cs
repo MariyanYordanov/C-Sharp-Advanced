6a028cc6a087c2680944a678e7b65ef7697b4a35
Mariyan Yordanov <marsrewq@gmail.com>
2022-06-23 05:46:34 +0300

    Add task

---
+++ 01.StacksAndQueues/LabStacksAndQueues/L.02.StackSum/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace L._02.StackSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] input = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            Stack<int> stack = new Stack<int>();
+            foreach (var item in input)
+            {
+                stack.Push(item);
+            }
+
+            string command = Console.ReadLine().ToLower();
+            while (command != "end")
+            {
+                string[] token = command.Split().ToArray();
+                if (token[0] == "add")
+                {
+                    int firstNum = int.Parse(token[1]);
+                    int secondtNum = int.Parse(token[2]);
+                    stack.Push(firstNum);
+                    stack.Push(secondtNum);
+                }
+                else if (token[0] == "remove")
+                {
+                    int removeNums = int.Parse(token[1]);
+                    if (removeNums < stack.Count)
+                    {
+                        for (int i = 0; i < removeNums; i++)
+                        {
+                            stack.Pop();
+                        }
+                    }
+                    
+                }
+
+                command = Console.ReadLine().ToLower();
+            }
+
+            Console.WriteLine($"Sum: {stack.Sum()}");
+        }
+    }
+}
