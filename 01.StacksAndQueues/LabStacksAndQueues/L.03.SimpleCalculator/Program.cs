2bae3096c1bf5d141b2cae8d4362b9db6d2e2d7c
Mariyan Yordanov <marsrewq@gmail.com>
2022-06-23 05:47:42 +0300

    Add task

---
+++ 01.StacksAndQueues/LabStacksAndQueues/L.03.SimpleCalculator/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace L._03.SimpleCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split(" ",StringSplitOptions.RemoveEmptyEntries).ToArray();
+            input = input.Reverse().ToArray();
+            Stack<string> stack = new Stack<string>();
+            foreach (string item in input)
+            {
+                stack.Push(item);
+            }
+
+            int result = int.Parse(stack.Peek());
+            stack.Pop();
+            string lastOperator = "";
+            while (stack.Count > 0)
+            {
+                string lastElement = stack.Peek();
+                if (lastElement != "+" && lastElement != "-")
+                {
+                    int lastDigit = int.Parse(lastElement);
+                    if (lastOperator == "-")
+                    {
+                        result -= lastDigit;
+                    }
+                    else
+                    {
+                        result += lastDigit;
+                    }
+                }
+                else if (lastElement == "-")
+                {
+                    lastOperator = "-";
+                }
+                else if (lastElement == "+")
+                {
+                    lastOperator = "+";
+                }
+
+                stack.Pop();
+            }
+
+            Console.WriteLine(result);
+        }
+    }
+}
