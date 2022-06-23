44e33bf2b63c5cea51fb3ad08ec46f95eb532567
Mariyan Yordanov <marsrewq@gmail.com>
2022-06-23 05:45:48 +0300

    Add task

---
+++ 01.StacksAndQueues/LabStacksAndQueues/L.01.Reverse Strings/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace L._01.Reverse_Strings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            char[] input = Console.ReadLine().ToCharArray();
+            Stack<char> myStack = new Stack<char>();
+            for (int i = 0; i < input.Length; i++)
+            {
+                myStack.Push(input[i]);
+            }
+
+            myStack.Reverse();
+
+            foreach (var item in myStack)
+            {
+                Console.Write(item);
+            }
+
+        }
+    }
+}
