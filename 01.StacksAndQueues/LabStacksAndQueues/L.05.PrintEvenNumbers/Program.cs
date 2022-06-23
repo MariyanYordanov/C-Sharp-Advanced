3438c1ce12357d3b742c75bc2c01ed23477a0f8f
Mariyan Yordanov <marsrewq@gmail.com>
2022-06-23 05:50:00 +0300

    Add task

---
+++ 01.StacksAndQueues/LabStacksAndQueues/L.05.PrintEvenNumbers/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace L._05.PrintEvenNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            Queue<int> queue = new Queue<int>(arr);
+
+            while (queue.Count > 0)
+            {
+                int element = queue.Dequeue();
+                if (element % 2 == 0)
+                {
+                    if (queue.Count == 0)
+                    {
+                        Console.Write(element);
+                    }
+                    else
+                    {
+                        Console.Write(element + ", ");
+                    }
+                }
+            }
+        }
+    }
+}
