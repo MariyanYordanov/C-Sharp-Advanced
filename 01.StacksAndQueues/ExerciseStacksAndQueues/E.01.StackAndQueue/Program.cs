dd8d6da9640d1cc506b202a99ecb2dbcc903835b
Mariyan Yordanov <marsrewq@gmail.com>
2022-06-24 06:55:17 +0300

    Add task

---
+++ 01.StacksAndQueues/ExerciseStacksAndQueues/E.01.StackAndQueue/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace E._01._Basic_Stack_Operations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] parameters = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int n = parameters[0];
+            int s = parameters[1];
+            int x = parameters[2];
+            int[] integers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            Stack<int> stack = new Stack<int>(integers);
+
+            for (int i = 0; i < s; i++)
+            {
+                stack.Pop();
+            }
+
+            if (stack.Contains(x))
+            {
+                Console.WriteLine("true");
+            }
+            else
+            {
+                if (stack.Count > 0)
+                {
+                    Console.WriteLine(stack.Min());
+                }
+                else
+                {
+                    Console.WriteLine(0);
+                }
+            }
+        }
+    }
+}
