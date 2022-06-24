2276f2af064e6fc79456b533664927724cf620cb
Mariyan Yordanov <marsrewq@gmail.com>
2022-06-24 06:58:07 +0300

    Add task

---
+++ 01.StacksAndQueues/ExerciseStacksAndQueues/E.04.FastFood/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace E._04.FastFood
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int quantityFood = int.Parse(Console.ReadLine());
+
+            Queue<int> orders = new Queue<int>(Console.ReadLine().Split().Select(int.Parse).ToArray());
+
+            Console.WriteLine(orders.Max());
+
+            while (orders.Count > 0)
+            {
+                if (quantityFood >= orders.Peek())
+                {
+                    int servedOrder = orders.Dequeue();
+                    quantityFood -= servedOrder;
+                }
+                else
+                {
+                    Console.WriteLine($"Orders left: {string.Join(" ", orders)}");
+                    return;
+                }
+            }
+
+            Console.WriteLine("Orders complete");
+        }
+    }
+}
