ac65b8d9c6a8c77bc0e4d6ef8522eed9a426fc53
Mariyan Yordanov <marsrewq@gmail.com>
2022-06-24 06:59:34 +0300

    Add tasks

---
+++ 01.StacksAndQueues/ExerciseStacksAndQueues/E.05.FashionBoutique/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace E._05.FashionBoutique
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<int> stack = new Stack<int>(Console.ReadLine().Split().Select(int.Parse).ToArray());
+            int capacityRack = int.Parse(Console.ReadLine());
+            int sum = 0;
+            int counter = 1;
+            while (stack.Count > 0)
+            {
+                sum += stack.Peek();
+                if (sum <= capacityRack)
+                {
+                    stack.Pop();
+                }
+                else
+                {
+                    sum = 0;
+                    counter++;
+                }
+            }
+
+            Console.WriteLine(counter);
+        }
+    }
+}
