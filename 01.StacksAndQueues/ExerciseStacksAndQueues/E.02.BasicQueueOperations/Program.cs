bd5f930e36a13fff184019bee558d515d1c7642c
Mariyan Yordanov <marsrewq@gmail.com>
2022-06-24 06:56:13 +0300

    Add task

---
+++ 01.StacksAndQueues/ExerciseStacksAndQueues/E.02.BasicQueueOperations/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace E._02.BasicQueueOperations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] parameters = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int n = parameters[0];
+            int s = parameters[1];
+            int x = parameters[2];
+            int[] integers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            Queue<int> queue = new Queue<int>(integers);
+
+            for (int i = 0; i < s; i++)
+            {
+                queue.Dequeue();
+            }
+
+            if (queue.Contains(x))
+            {
+                Console.WriteLine("true");
+            }
+            else
+            {
+                if (queue.Count > 0)
+                {
+                    Console.WriteLine(queue.Min());
+                }
+                else
+                {
+                    Console.WriteLine(0);
+                }
+            }
+        }
+    }
+}
