e69d5b163ae26b161143a80e5c372078f87cf86b
Mariyan Yordanov <marsrewq@gmail.com>
2022-06-25 11:21:52 +0300

    Add task

---
+++ 01.StacksAndQueues/ExerciseStacksAndQueues/E.07.TruckTour/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+
+namespace E._07.TruckTour
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] pump = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                int[] data = Console.ReadLine().Split().Select(int.Parse).ToArray();
+                pump[i] = data[0] - data[1];
+            }
+
+            int current = 0;
+            int position = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                current += pump[i];
+
+                if (current < 0)
+                {
+                    current = 0;
+                    position = i + 1;
+                }
+            }
+            Console.WriteLine(position);
+        }
+    }
+}
