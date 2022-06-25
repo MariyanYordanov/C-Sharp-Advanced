d3181050f057b126283ea32793c92e27ce749a6f
Mariyan Yordanov <marsrewq@gmail.com>
2022-06-25 11:22:58 +0300

    Add task

---
+++ 01.StacksAndQueues/ExerciseStacksAndQueues/E.08.Balanced Parentheses/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace E._08.Balanced_Parentheses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] charArr = Console.ReadLine().ToCharArray();
+            Stack<char> stack = new Stack<char>();
+            for (int i = 0; i < charArr.Length; i++)
+            {
+                char currentChar = charArr[i];
+                if (currentChar == '{' || currentChar == '[' || currentChar == '(')
+                {
+                    stack.Push(currentChar);
+                }
+                else if (currentChar == '}' || currentChar == ']' || currentChar == ')')
+                {
+                    if (stack.Count == 0)
+                    {
+                        Console.WriteLine("NO");
+                        return; 
+                    }
+                    char topChar = stack.Peek();
+                    if ((topChar == '{' && currentChar == '}') || (topChar == '[' && currentChar == ']') || (topChar == '(' && currentChar == ')'))
+                    {
+                        stack.Pop();
+                    }
+                    else 
+                    { 
+                        break; 
+                    }
+                }
+            }
+
+            Console.WriteLine(stack.Count > 0 ? "NO" : "YES");
+        }
+
+    }
+}
