cb445a6a8c28b52a1c056a02370304e06384a3c1
Mariyan Yordanov <marsrewq@gmail.com>
2022-06-28 00:11:10 +0300

    add task

---
+++ 02.MultidimentionalArrays/1.LabMultidimenntionalArrays/L.06.Jagged-ArrayModification/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Linq;
+
+namespace L._06.Jagged_ArrayModification
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[][] jagg = new int[n][];
+            for (int row = 0; row < jagg.Length; row++)
+            {
+                int[] inputRow = Console.ReadLine().Split().Select(int.Parse).ToArray();
+                jagg[row] = new int[inputRow.Length];
+                for (int col = 0; col < jagg[row].Length; col++)
+                {
+                    jagg[row][col] = inputRow[col];
+                }
+            }
+
+            string[] command = Console.ReadLine().Split(); 
+            while (command[0] != "END")
+            {
+                int row = int.Parse(command[1]);
+                int col = int.Parse(command[2]);
+                int value = int.Parse(command[3]);
+                if (row < 0 || col < 0 || row > jagg.Length - 1 || col > jagg.Length - 1)
+                {
+                    Console.WriteLine("Invalid coordinates");
+                }
+                else
+                {
+                    if (command[0] == "Add")
+                    {
+                        jagg[row][col] += value;
+                    }
+                    else if (command[0] == "Subtract")
+                    {
+                        jagg[row][col] -= value;
+                    }
+                }
+                
+                command = Console.ReadLine().Split();
+            }
+
+            for (int row = 0; row < jagg.Length; row++)
+            {
+                for (int col = 0; col < jagg[row].Length; col++)
+                {
+                    Console.Write(jagg[row][ col] + " ");
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
