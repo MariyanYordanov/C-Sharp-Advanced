0b1cb17b40ac3b1aca16ff09531bba4de657b833
Mariyan Yordanov <marsrewq@gmail.com>
2022-06-28 00:06:11 +0300

    Add task

---
+++ 02.MultidimentionalArrays/1.LabMultidimenntionalArrays/L.01.SumMatrixElements/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Linq;
+
+namespace L._01.SumMatrixElements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] input = Console.ReadLine().Split(", ").Select(int.Parse).ToArray();
+            int rows = input[0];
+            int cols = input[1];
+            int[,] matrix = new int[rows,cols];
+            int sum = 0;
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                int[] rowInput = Console.ReadLine().Split(", ").Select(int.Parse).ToArray();
+                
+                for (int col = 0; col < rowInput.Length; col++)
+                {
+                    matrix[row, col] = rowInput[col];
+                    sum += rowInput[col];
+                }
+                
+            }
+
+            Console.WriteLine(rows);
+            Console.WriteLine(cols);
+            Console.WriteLine(sum);
+
+
+            // PRINT   MATRIX
+
+            //for (int row = 0; row < matrix.GetLength(0); row++)
+            //{
+            //    for (int col = 0; col < matrix.GetLength(1); col++)
+            //    {
+            //        Console.Write(matrix[row, col] + " ");
+            //    }
+
+            //    Console.WriteLine();
+            //}
+        }
+    }
+}
