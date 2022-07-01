83d4842f569a3bdf53cf30798eab63e9a18e8a3b
Mariyan Yordanov <marsrewq@gmail.com>
2022-07-01 09:33:18 +0300

    Add task

---
+++ 02.MultidimentionalArrays/2.ExerciseMultidimentionalarrays/E.01.DiagonalDifference/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+
+namespace E._01.DiagonalDifference
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[,] matrix = new int[n, n];
+            int sumPrimeryDiagonal = 0;
+            int sumSecondaryDiagonal = 0;
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                int[] rowInput = Console.ReadLine().Split().Select(int.Parse).ToArray();
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    matrix[row, col] = rowInput[col];
+                    if (row == col)
+                    {
+                        sumPrimeryDiagonal += matrix[row, col];
+                    }
+                    if (n - 1 - row == col)
+                    {
+                        sumSecondaryDiagonal += matrix[row, col];
+                    }
+                }
+            }
+
+            Console.WriteLine(Math.Abs(sumPrimeryDiagonal - sumSecondaryDiagonal));
+            
+        }
+    }
+}
