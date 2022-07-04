e15086ebccaa6d9967c7b5936bfe3fa3959b087d
Mariyan Yordanov <marsrewq@gmail.com>
2022-07-04 22:39:38 +0300

    Add task

---
+++ 03.SetsAndDictionary/E.02.SetsOfElements/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace E._02.SetsOfElements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<int> first = new HashSet<int>();
+            HashSet<int> second = new HashSet<int>();
+            int[] input = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int n = input[0];
+            int m = input[1];
+            for (int i = 0; i < n; i++)
+            {
+                int num = int.Parse(Console.ReadLine());
+                first.Add(num);
+            }
+
+            for (int i = 0; i < m; i++)
+            {
+                int num = int.Parse(Console.ReadLine());
+                second.Add(num);
+            }
+
+            List<int> result = first.Intersect(second).ToList();
+
+            Console.WriteLine(string.Join(" ",result));
+        }
+    }
+}
