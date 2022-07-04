f947de8b87c649dc3e670f4dedb1533c3240017d
Mariyan Yordanov <marsrewq@gmail.com>
2022-07-04 22:40:24 +0300

    Add task

---
+++ 03.SetsAndDictionary/E.03.PeriodicTable/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace E._03.PeriodicTable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<string> elements = new HashSet<string>();
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                string[] element = Console.ReadLine().Split();
+                for (int j = 0; j < element.Length; j++)
+                {
+                    elements.Add(element[j]);
+                }
+            }
+            elements = elements.OrderBy(x => x).ToHashSet();
+            Console.WriteLine(string.Join(" ", elements));
+        }
+    }
+}
