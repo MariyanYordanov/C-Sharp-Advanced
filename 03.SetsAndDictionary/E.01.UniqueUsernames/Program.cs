f56d9db42cf46e0db8011ce396f334155ca21eed
Mariyan Yordanov <marsrewq@gmail.com>
2022-07-04 22:38:34 +0300

    Add task

---
+++ 03.SetsAndDictionary/E.01.UniqueUsernames/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace E._01.UniqueUsernames
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<string> usernames = new HashSet<string>();
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                string input = Console.ReadLine();
+                usernames.Add(input);
+            }
+
+            usernames.ToList().ForEach(x => Console.WriteLine(x));
+        }
+    }
+}
