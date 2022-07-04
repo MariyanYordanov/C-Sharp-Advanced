c9477b296a624dc32147f12f365cb29cf1f65321
Mariyan Yordanov <marsrewq@gmail.com>
2022-07-04 22:41:55 +0300

    add task

---
+++ 03.SetsAndDictionary/E.05.CountSymbols/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace E._05.CountSymbols
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<char, int> charDic = new Dictionary<char, int>();
+            char[] input = Console.ReadLine().ToCharArray();
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (!charDic.ContainsKey(input[i]))
+                {
+                    charDic.Add(input[i],0);
+                }
+
+                charDic[input[i]]++;
+            }
+
+            charDic = charDic.OrderBy(x => x.Key).ToDictionary(x => x.Key, y => y.Value);
+            foreach (var item in charDic)
+            {
+                Console.WriteLine($"{item.Key}: {item.Value} time/s");
+            }
+        }
+    }
+}
