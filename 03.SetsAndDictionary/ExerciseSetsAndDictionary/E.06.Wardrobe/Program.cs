4339e4f92abf85249f7d38cfb556437f78fea94f
Mariyan Yordanov <marsrewq@gmail.com>
2022-07-05 23:17:59 +0300

    Add

---
+++ 03.SetsAndDictionary/ExerciseSetsAndDictionary/E.06.Wardrobe/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace E._06.Wardrobe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, Dictionary<string, int>> wardrobe = new Dictionary<string, Dictionary<string, int>>();
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                string[] input = Console.ReadLine().Split(" -> ");
+                string color = input[0];
+                string[] items = input[1].Split(",");
+                for (int j = 0; j < items.Length; j++)
+                {
+                    if (!wardrobe.ContainsKey(color))
+                    {
+                        wardrobe.Add(color, new Dictionary<string, int>());
+                    }
+
+                    if (!wardrobe[color].ContainsKey(items[j]))
+                    {
+                        wardrobe[color].Add(items[j],0);
+                    }
+
+                    wardrobe[color][items[j]]++;
+                }
+            }
+
+            string[] lookingForItem = Console.ReadLine().Split();
+            
+            foreach (var color in wardrobe)
+            {
+                Console.WriteLine($"{color.Key} clothes:");
+                foreach (var item in color.Value)
+                {
+                    if(item.Key == lookingForItem[1])
+                    {
+                        Console.WriteLine($"* {item.Key} - {item.Value} (found!)");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"* {item.Key} - {item.Value}");
+                    }
+                }
+            }
+        }
+    }
+}
