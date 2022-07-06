db32e9c13a3e755d87ad32a33ddb642e2da85b5e
Mariyan Yordanov <marsrewq@gmail.com>
2022-07-06 21:00:45 +0300

    Ad task

---
+++ 03.SetsAndDictionary/LabSetsAndDictionary/SetsAndDictionary/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SetsAndDictionaryadvans
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
