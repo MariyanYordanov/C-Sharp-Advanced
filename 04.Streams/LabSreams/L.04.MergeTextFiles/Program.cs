cd02774ef295daf49e74020aad52d2bed6942817
Mariyan Yordanov <marsrewq@gmail.com>
2022-07-07 00:54:45 +0300

    Add task

---
+++ 04.Streams/LabSreams/L.04.MergeTextFiles/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+
+namespace MergeFiles
+{
+    public class MergeFiles
+    {
+        static void Main(string[] args)
+        {
+            string firstInputFilePath = @"..\..\..\input1.txt";
+            string secondInputFilePath = @"..\..\..\input2.txt";
+            string outputFilePath = @"..\..\..\output.txt";
+            MergeTextFiles(firstInputFilePath, secondInputFilePath, outputFilePath);
+        }
+        public static void MergeTextFiles(string firstInputFilePath, string
+        secondInputFilePath, string outputFilePath)
+        {
+            using StreamReader readerFirst = new StreamReader(firstInputFilePath);
+            using StreamReader readerSecond = new StreamReader(secondInputFilePath);
+            using StreamWriter writer = new StreamWriter(outputFilePath);
+            string first = readerFirst.ReadLine();
+            string second = readerSecond.ReadLine();
+            while (first != null)
+            {
+                writer.WriteLine($"{first}\n{second}");
+                first = readerFirst.ReadLine();
+                second = readerSecond.ReadLine();
+            }
+
+        }
+    }
+}
