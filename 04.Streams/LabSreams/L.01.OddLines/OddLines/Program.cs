cdae2a10f68f2ef5525c7ee6aaa175ef1d0755c4
Mariyan Yordanov <marsrewq@gmail.com>
2022-07-07 00:51:32 +0300

    Add task

---
+++ 04.Streams/LabSreams/L.01.OddLines/OddLines/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace OddLines
+{
+    public class OddLines
+    {
+        static void Main()
+        {
+
+            string inputFile = @"input.txt";
+            string outputFile = @"..\..\..\output.txt";
+            ExtractOddLines(inputFile, outputFile);
+        }
+        public static void ExtractOddLines(string inputFilePath, string outputFilePath)
+        {
+            using StreamReader str = new StreamReader(inputFilePath);
+            using StreamWriter sw = new StreamWriter(outputFilePath);
+
+            int lineCounter = 0;
+            string line = str.ReadLine();
+            while (line != null)
+            {
+                if (lineCounter % 2 != 0)
+                {
+                    sw.WriteLine(line);
+                }
+                lineCounter++;
+                line = str.ReadLine();
+            }
+        }
+    }
+}
