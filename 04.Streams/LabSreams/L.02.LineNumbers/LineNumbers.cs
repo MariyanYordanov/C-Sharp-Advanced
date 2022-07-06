ef66a41af7ec614f634ce882155508f6a367bcc4
Mariyan Yordanov <marsrewq@gmail.com>
2022-07-07 00:52:40 +0300

    Add task

---
+++ 04.Streams/LabSreams/L.02.LineNumbers/LineNumbers.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+
+namespace LineNumbers
+{
+    public class LineNumbers
+    {
+        static void Main()
+        {
+            string inputFilePath = @"..\..\..\Files\input.txt";
+            string outputFilePath = @"..\..\..\Files\output.txt";
+            RewriteFileWithLineNumbers(inputFilePath, outputFilePath);
+        }
+        public static void RewriteFileWithLineNumbers(string inputFilePath, string
+        outputFilePath)
+        {
+            using (var reader = new StreamReader(inputFilePath))
+            {
+                string line = reader.ReadLine();
+                int counter = 1;
+                using (var writer = new StreamWriter(outputFilePath))
+                {
+                    while (line != null)
+                    {
+                        writer.WriteLine($"{counter}. {line}");
+                        line = reader.ReadLine();
+                        counter++;
+
+                    }
+                }
+            }
+        }
+    }
+}
