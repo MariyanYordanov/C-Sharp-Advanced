7d73a71b0ef72e5262da891b913272759de84484
Mariyan Yordanov <marsrewq@gmail.com>
2022-07-06 20:48:42 +0300

    Add task

---
+++ 03.SetsAndDictionary/LabSetsAndDictionary/L.04.ProductShop/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace L._04.ProductShop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, Dictionary<string, double>> markets = new Dictionary<string, Dictionary<string, double>>();
+            string input = Console.ReadLine();
+            while (input != "Revision")
+            {
+                string[] splited = input.Split(", ").ToArray();
+                string market = splited[0];
+                string product = splited[1];
+                double price = double.Parse(splited[2]);
+                if (!markets.ContainsKey(market))
+                {
+                    markets.Add(market, new Dictionary<string, double>());
+                }
+
+                markets[market].Add(product, price);
+
+                input = Console.ReadLine();
+            }
+
+            markets = markets.OrderBy(x => x.Key).ToDictionary(x=> x.Key, y => y.Value);
+            foreach (var market in markets)
+            {
+                Console.WriteLine($"{market.Key}->");
+                foreach (var product in market.Value)
+                {
+                    Console.WriteLine($"Product: {product.Key}, Price: {product.Value}");
+                }
+            }
+        }
+    }
+}
