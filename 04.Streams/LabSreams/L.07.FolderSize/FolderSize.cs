f448d41ead0cc5306bfdf7f58c126e2eb1dc893d
Mariyan Yordanov <marsrewq@gmail.com>
2022-07-12 21:11:16 +0300

    Add task

---
+++ 04.Streams/LabSreams/L.07.FolderSize/FolderSize.cs
@@ -0,0 +1,29 @@
+﻿namespace FolderSize
+{
+    using System;
+    using System.IO;
+
+    public class FolderSize
+    {
+        static void Main(string[] args)
+        {
+            string folderPath = @"..\..\..\TestFolder";
+            string outputFilePath = @"..\..\..\output.txt";
+            GetFolderSize(folderPath, outputFilePath);
+        }
+
+        public static void GetFolderSize(string folderPath, string outputFilePath)
+        {
+            double sum = 0;
+            DirectoryInfo dir = new DirectoryInfo(folderPath);
+            FileInfo[] infos = dir.GetFiles("*", SearchOption.AllDirectories);
+            foreach (FileInfo fileInfo in infos)
+            {
+                sum += fileInfo.Length;
+            }
+
+            sum = sum/1024;
+            File.WriteAllText(outputFilePath, sum.ToString() + " KB");
+        }
+    }
+}
