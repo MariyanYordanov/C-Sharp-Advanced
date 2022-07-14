459c987ed3a679a13522972dc620d04719176a45
Mariyan Yordanov <marsrewq@gmail.com>
2022-07-14 20:49:29 +0300

    Add task

---
+++ 05.FunctionalProgramming/LabFunctionalProgramming/L.04.AddVAT/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+
+namespace L._04.AddVAT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] numbers = Console.ReadLine().Split(", ").Select(double.Parse).Select(x => x *= 1.2).ToArray();
+            foreach (var number in numbers)
+            {
+                Console.WriteLine($"{number:f2}");
+            }
+        }
+    }
+}
