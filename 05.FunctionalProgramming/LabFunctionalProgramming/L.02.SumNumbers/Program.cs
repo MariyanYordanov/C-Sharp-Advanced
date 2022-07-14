d8ca072a7d69bade542e86615022d7859c86046e
Mariyan Yordanov <marsrewq@gmail.com>
2022-07-14 20:47:54 +0300

    add task

---
+++ 05.FunctionalProgramming/LabFunctionalProgramming/L.02.SumNumbers/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+
+namespace L._02.SumNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().Split(", ").Select(int.Parse).ToArray();
+            Console.WriteLine(numbers.Count());
+            Console.WriteLine(numbers.Sum());
+        }
+    }
+}
