05ede81fb5f2756fb577d74ac5ad4cbd48935a40
Mariyan Yordanov <marsrewq@gmail.com>
2022-07-15 10:23:48 +0300

    Add task

---
+++ 05.FunctionalProgramming/ExerciseFunctionalProgramming/E.02.KnightsOfHonor/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace E._02.KnightsOfHonor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> names = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).ToList();
+            Action<List<string>> print = names => names.ForEach(x => Console.WriteLine($"Sir {x}"));
+            print(names);
+        }
+    }
+}
