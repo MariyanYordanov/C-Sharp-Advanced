7ffe316f3fe51361025574f6d8edd95c0c72f772
Mariyan Yordanov <marsrewq@gmail.com>
2022-07-15 10:22:57 +0300

    Add task

---
+++ 05.FunctionalProgramming/ExerciseFunctionalProgramming/E.01.ActionPoint/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace E._01.ActionPoint
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] names = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            Action<string[]> print = names => Console.WriteLine(string.Join("\n", names));
+            print(names);
+        }
+    }
+}
