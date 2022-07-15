1e7bee9dfe2a5afa816155f49cad395b9b10c296
Mariyan Yordanov <marsrewq@gmail.com>
2022-07-15 10:24:59 +0300

    Add task

---
+++ 05.FunctionalProgramming/ExerciseFunctionalProgramming/E.03.CustomMinFunction/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+
+namespace E._03.CustomMinFunction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            Func<int[], int> minimum = x =>
+            {
+                int minValue = int.MaxValue;
+                for (int i = 0; i < numbers.Length; i++)
+                {
+                    if (numbers[i] < minValue)
+                    {
+                        minValue = numbers[i];
+                    }
+                }
+
+                return minValue;
+            };
+
+            Console.WriteLine(minimum(numbers));
+        }
+
+    }
+}
