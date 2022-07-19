88c6ce97061c15d931b79fcd38c9dffa15698b39
Mariyan Yordanov <marsrewq@gmail.com>
2022-07-19 21:42:05 +0300

    Add task

---
+++ 05.FunctionalProgramming/ExerciseFunctionalProgramming/E.04.EvenOrOddSum/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+
+namespace E._04.EvenOrOddSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] interval = Console.ReadLine().Split(" ",StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            int start = interval[0];
+            int end = interval[1];
+            string operation = Console.ReadLine();
+            Predicate<int> isEven = number => number % 2 == 0;
+            Predicate<int> isOdd = number => number % 2 != 0;
+            for (int i = start; i <= end; i++)
+            {
+                if (operation == "odd" && isOdd(i))
+                {
+                    Console.Write(i + " ");
+                }
+                else if(operation == "even" && isEven(i))
+                {
+                    Console.Write(i + " ");
+                }
+            }
+        }
+    }
+}
