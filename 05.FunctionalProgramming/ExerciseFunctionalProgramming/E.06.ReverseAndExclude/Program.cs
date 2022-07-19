749eca46b24bfe71b27b4ecb9eeb802c06de6ec0
Mariyan Yordanov <marsrewq@gmail.com>
2022-07-19 21:44:26 +0300

    Add task

---
+++ 05.FunctionalProgramming/ExerciseFunctionalProgramming/E.06.ReverseAndExclude/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace E._06.ReverseAndExclude
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<int, int, bool> isDivisible = (x, y) => x % y == 0;
+            int[] numbers = Console.ReadLine().Split().Select(int.Parse).Reverse().ToArray();
+            int y = int.Parse(Console.ReadLine());
+            List<int> result = numbers.Where(x => !isDivisible(x,y)).ToList();
+            Console.WriteLine(string.Join(" ", result));
+        }
+    }
+}
