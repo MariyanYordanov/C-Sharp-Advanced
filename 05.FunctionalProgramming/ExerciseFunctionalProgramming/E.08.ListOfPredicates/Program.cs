00709e7c68e08d20fbd5e4e5d1154a8bec1b934c
Mariyan Yordanov <marsrewq@gmail.com>
2022-07-19 21:46:52 +0300

    Add task

---
+++ 05.FunctionalProgramming/ExerciseFunctionalProgramming/E.08.ListOfPredicates/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+
+namespace E._08.ListOfPredicates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<int, int, bool> isDiv = (number, divider) => number % divider == 0;
+            int n = int.Parse(Console.ReadLine());
+            int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            for (int i = 1; i <= n; i++)
+            {
+                bool isDivider = false;
+                foreach (var number in numbers)
+                {
+                    if (isDiv(i, number))
+                    {
+                        isDivider = true;
+                    }
+                    else
+                    {
+                        isDivider = false;
+                        break;
+                    }
+                }
+
+                if (isDivider)
+                {
+                    Console.Write(i + " ");
+                }
+            }
+        }
+    }
+}
