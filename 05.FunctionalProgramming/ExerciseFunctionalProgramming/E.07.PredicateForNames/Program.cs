d5ed1c630109d1660fc4d3d5ef2f1c19845f4ace
Mariyan Yordanov <marsrewq@gmail.com>
2022-07-19 21:45:32 +0300

    Add task

---
+++ 05.FunctionalProgramming/ExerciseFunctionalProgramming/E.07.PredicateForNames/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace E._07.PredicateForNames
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<string, int, bool> isLongEnough = (name, lenght) => name.Length <= lenght;
+            int n = int.Parse(Console.ReadLine());
+            string[] names = Console.ReadLine().Split();
+            foreach (var name in names)
+            {
+                if (isLongEnough(name, n))
+                {
+                    Console.WriteLine(name);
+                }
+            }
+            
+        }
+    }
+}
