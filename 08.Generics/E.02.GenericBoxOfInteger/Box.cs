e1dfa7c0ae6ee91c0d21fa1e08bab97dc2d6c7e6
Mariyan Yordanov <marsrewq@gmail.com>
2022-07-24 23:45:54 +0300

    Add task

---
+++ 08.Generics/E.02.GenericBoxOfInteger/Box.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace E._02.GenericBoxOfInteger
+{
+    public class Box<T>
+    {
+        public List<T> Items;
+
+        public Box()
+        {
+            Items = new List<T>();
+        }
+
+        public override string ToString()
+            => string.Join(Environment.NewLine, Items.Select(x => $"{typeof(T)}: {x}"));
+        
+    }
+}
---
+++ 08.Generics/E.02.GenericBoxOfInteger/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace E._02.GenericBoxOfInteger
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Box<int> numbers = new Box<int>();
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                int input = int.Parse(Console.ReadLine());
+                numbers.Items.Add(input);
+            }
+
+            Console.WriteLine(numbers);
+        }
+    }
+}
