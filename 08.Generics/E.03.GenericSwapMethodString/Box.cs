46ed6abb10134264478fb182d16c76687d6d1b01
Mariyan Yordanov <marsrewq@gmail.com>
2022-07-24 23:48:05 +0300

    Add task

---
+++ 08.Generics/E.03.GenericSwapMethodString/Box.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace E._03.GenericSwapMethodString
+{
+    public class Box<T>
+    {
+        public List<T> Items;
+
+        public Box()
+        {
+            Items = new List<T>();
+        }
+
+        public override string ToString()
+            => string.Join(Environment.NewLine, Items.Select(x => $"{typeof(T)}: {x}"));
+
+        /// <summary>
+        /// Swap on one line
+        /// </summary>
+        /// <param name="left"></param>
+        /// <param name="right"></param>
+        public void Swap(int left, int right)
+            => (Items[left], Items[right]) = (Items[right], Items[left]);
+        //{
+        //    T temp = Items[left];
+        //    Items[left] = Items[right];
+        //    Items[right] = temp;
+        //}
+    }
+}
---
+++ 08.Generics/E.03.GenericSwapMethodString/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace E._03.GenericSwapMethodString
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Box<string> box = new Box<string>();
+
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                string input = Console.ReadLine();
+                box.Items.Add(input);
+            }
+
+            string[] commands = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            int firstCmd = int.Parse(commands[0]);
+            int secondCmd = int.Parse(commands[1]);
+            box.Swap(firstCmd, secondCmd);
+            Console.WriteLine(box.ToString());
+        }
+    }
+}
