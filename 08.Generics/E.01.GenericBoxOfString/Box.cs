6e67e8f45ed174b4d840208d43411f3b7c14584a
Mariyan Yordanov <marsrewq@gmail.com>
2022-07-24 23:44:51 +0300

    Add task

---
+++ 08.Generics/E.01.GenericBoxOfString/Box.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace E._01.GenericBoxOfString
+{
+    public class Box<T>
+    {
+        public List<T> Items { get; set; }
+
+        public Box()
+        {
+            Items = new List<T>();
+        }
+
+        public override string ToString()
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+            foreach (var item in Items)
+            {
+                stringBuilder.AppendLine($"{typeof(T)}: {item}");
+            }
+
+            return stringBuilder.ToString();
+        }
+    }
+}
