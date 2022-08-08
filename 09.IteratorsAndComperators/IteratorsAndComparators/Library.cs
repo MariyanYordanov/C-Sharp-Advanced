6414092c4ab53bdf0443ed4896f097dbc2a57c39
Mariyan Yordanov <marsrewq@gmail.com>
2022-08-08 08:04:24 +0300

    add theme

---
+++ 09.IteratorsAndComperators/IteratorsAndComparators/Library.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IteratorsAndComparators
+{
+    public class Library<Book> : IEnumerable<Book>
+    {
+        private List<Book> books;
+
+        public Library(params Book[] books)
+        {
+            this.books = new List<Book>(books);
+        }
+
+        public IEnumerator<Book> GetEnumerator()
+        {
+            for (int i = 0; i < books.Count; i++)
+            {
+                yield return books[i];
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() => this.GetEnumerator();
+
+    }
+}
