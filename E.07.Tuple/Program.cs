24f96d879ae184c861c709e68bafedf9b4eef136
Mariyan Yordanov <marsrewq@gmail.com>
2022-08-08 06:42:42 +0300

    add task

---
+++ E.07.Tuple/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace E._07.Tuple
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] personInfo = Console.ReadLine().Split();
+            string[] beerInfo = Console.ReadLine().Split();
+            string[] numbersInfo = Console.ReadLine().Split();
+
+            MyTuple<string, string> personTuple = new MyTuple<string, string>
+                ($"{personInfo[0]} {personInfo[1]}", personInfo[2]);
+
+            MyTuple<string, int> beerTuple = new MyTuple<string, int>
+                ($"{beerInfo[0]}", int.Parse(beerInfo[1]));
+
+            MyTuple<int, double> numbersTyple = new MyTuple<int, double>
+                (int.Parse(numbersInfo[0]), double.Parse(numbersInfo[1]));
+
+            Console.WriteLine(personTuple.ToString());
+            Console.WriteLine(beerTuple.ToString());
+            Console.WriteLine(numbersTyple.ToString());
+        }
+    }
+}
