8aab6e5446d63f952d316e9deff472d39661b2b8
Mariyan Yordanov <marsrewq@gmail.com>
2022-08-08 06:41:40 +0300

    add task

---
+++ 08.Generics/E.06.GenericCountMethodDouble/Box.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace E._06.GenericCountMethodDouble
+{
+    public class Box<T>
+        where T : IComparable
+    {
+        public List<T> Items;
+
+        public Box()
+        {
+            Items = new List<T>();
+        }
+
+        public int CountBiggerThanInput(T input)
+            => Items.Count(x => x.CompareTo(input) > 0); 
+        
+    }
+}
