77670a12c1b2a8dd18b5b465ca247bd23b8dabf1
Mariyan Yordanov <marsrewq@gmail.com>
2022-09-06 22:07:48 +0300

    Add Exam

---
+++ ExamAdvanced/ExamAdvance/Exception.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Runtime;
+namespace Exeption
+{
+    class Exception
+    {
+        static void Main(string[] args)
+        {
+            double number = default;
+            try
+            {
+                string line = Console.ReadLine();
+                bool tryDouble = double.TryParse(line, out number);
+                number = Math.Sqrt(number);
+            }
+            catch (ArgumentNullException)
+            {
+                if (number != default)
+                {
+                    throw new ArgumentNullException("Invalid number");
+                }
+            }
+            catch (ArgumentOutOfRangeException)
+            {
+                if (number <= 0)
+                {
+                    throw new ArgumentOutOfRangeException("Invalid number");
+                }
+            }
+            finally
+            {
+                Console.WriteLine(number);
+                Console.WriteLine("Good Bye");
+            }
+        }
+    }
+}
